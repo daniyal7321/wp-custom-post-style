9909a63a4c5495c9c7a61598aaee7d277129d98d
shahzaibtech <92725187+shahzaibtech@users.noreply.github.com>
2021-12-24 12:27:33 +0500

    changings

--- wp-custom-post-style.php
+++ wp-custom-post-style.php
@@ -31,19 +31,3 @@ function on_deactivating_your_plugin() {
 }
 register_deactivation_hook( __FILE__, 'on_deactivating_your_plugin' );
 // Register Post Type
-function pluginprefix_setup_post_type() {
-    register_post_type( 'book', ['public' => true ] ); 
-} 
-add_action( 'init', 'pluginprefix_setup_post_type' );
-// Plugin Activation
-function wp_custom_post_style_activate() {
-    pluginprefix_setup_post_type();
-    flush_rewrite_rules(); 
-}
-register_activation_hook( 'wp-custom-post-style', 'wp_custom_post_style_activate' );
-// Plugin Deactivation
-function wp_custom_post_style_deactivate() {
-    unregister_post_type( 'book' );
-    flush_rewrite_rules();
-}
-register_deactivation_hook( 'wp-custom-post-style', 'wp_custom_post_style_deactivate' );
